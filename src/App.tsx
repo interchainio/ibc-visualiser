0efcc802070f1e4e1b0464d2479effbd6e0f5101
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2021-05-10 07:06:35 -0400

    feat add: Table navigation elements

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 // Components 
 import RootNavBar from './components/Navigation/RootNav/RootNavBar'
+import WebMenu from './components/Navigation/Menu/WebMenu'
 
 const App = () => {
   return (
     <Router>
       <div className="App">
         <RootNavBar/>
+        <WebMenu/>
         {/* Switch : Body Nav */}
         <Switch>
           {/* <Route exact path='/' component={() => <WelcomeToIBCViz/>}/> */}
