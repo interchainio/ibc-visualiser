01994eafe6e623494281c9dd5b360e0d4692a578
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2021-05-10 08:00:22 -0400

    feat: Move navigation to Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,22 @@ import * as React from 'react';
 import { HashRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 // Components 
+import Dashboard from './components/Dashboard/Dashboard';
 import RootNavBar from './components/Navigation/RootNav/RootNavBar';
 import WebMenu from './components/Navigation/Menu/WebMenu';
 
 const App = () => {
 
+  const orgName = 'ibc'
+  const chainName = 'gaia'
+
   return (
     <Router>
       <div className='App'>
-        <RootNavBar/>
-        <WebMenu/>
-        {/* Switch : Body Nav */}
         <Switch>
           {/* <Route exact path='/' component={() => <WelcomeToIBCViz/>}/> */}
+          {/* switch path to: path={`/${orgName}/${chainName}`} */}
+          <Route exact path='/' component={() => <Dashboard/>}/>
         </Switch>
       </div>
     </Router>
